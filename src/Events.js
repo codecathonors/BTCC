a40bbfd0d3e40ed7400fa41d6bcc04076d279925
CatherineHonors <cathonors@gmail.com>
2022-11-16 15:06:36 -0500

    successfully retrieving single repo and single event data

--- src/Events.js
+++ src/Events.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import SingleEvent from "./SingleEvent";
 
 function Events() {
     const [events, setEvents] = useState([])
@@ -10,7 +11,7 @@ function Events() {
     }, []);
 
 
-    // console.log(events[0])
+    console.log(events[0])
 
     // function eventLoop() {
     //     for (let i = 0; i <= events.length; i++) {
@@ -25,59 +26,16 @@ function Events() {
     //         console.log(`${key}: ${event[key]}`)
     //     }
     // })
+    const singleEvent = events.map((event) => (
+        <SingleEvent event={event}/>
+    ))
+
   
 
     return (
-        // <div>
-        //     <h1> I'm events page</h1>
-        //     {events.map(event => {
-        //         return <div class="card">
-        //             <p>event: {event.type}</p>
-        //             <p>id: {event.id}</p>
-        //             <div class="card">
-        //                 <h6>Actor</h6>
-        //                 <img class="card-img-top" src={event.actor.avatar_url} alt="Various avatars" style={{ height: 100, width: 100 }}></img>
-        //                 <p>ID {event.actor.id}</p>
-        //                 <p>Login {event.actor.login}</p>
-        //                 <p>Display login {event.actor.display_login}</p>
-        //                 <p>Gravatar ID {event.actor.gravatar_id}</p>
-        //                 <p>URL {event.actor.url}</p>
-        //             </div>
-        //             <div class="card">
-        //                 <h6>Repo</h6>
-        //                 <p>ID {event.repo.id}</p>
-        //                 <p>Name {event.repo.name}</p>
-        //                 <p>URL {event.repo.url}</p>
-        //             </div>
-        //             <div class="card">
-        //                 <h6>Payload</h6>
-        //                 <p>Action {event.payload.action}</p>
-        //                 <p>Push ID {event.payload.push_id}</p>
-        //                 <p>Size {event.payload.size}</p>
-        //                 <p>Distinct Size {event.payload.distinct_size}</p>
-        //                 <p>Ref {event.payload.ref}</p>
-        //                 <p>Head {event.payload.head}</p>
-        //                 <p>Before {event.payload.before}</p>
-        //                 <div class="card">
-        //                     <h6>Commits</h6>
-        //                     {/* <p>sha: {event.payload.commits.map(sha => sha}</p> */}
-        //                 </div>
-        //                 {/* //come back to commits, very nested */}
-        //             </div>
-        //             <p>public: {event.public}</p>
-        //             <p>created: {event.created_at}</p>
-        //             <div class="card">
-        //                 <h6>Organization</h6>
-        //                 <p>ID {event.org.id}</p>
-        //                 <img class="card-img-top" src={event.org.avatar_url} alt="Boomtown blue logo" style={{ height: 100, width: 100 }}></img>
-        //                 <p>Login {event.org.login}</p>
-        //                 <p>Gravatar ID {event.org.gravatar_id}</p>
-        //                 <p>URL {event.org.url}</p>
-        //             </div>
-        //         </div>
-        //     })}
-        // </div>
-        <h1>hey</h1>
+        <div>
+            {singleEvent}
+        </div>
     )
 
 
---
+++ src/SingleRepo.js
@@ -0,0 +1,139 @@
+import React from "react";
+
+function SingleRepo( { repo }) {
+
+
+
+
+    // const singleEvent = events.map((event) => (
+    //     <SingleEvent event={event} />
+    // ))
+
+
+
+    return (
+        <div class="card">
+            <h1>{repo.name} | {repo.full_name}</h1>
+            <div class="card-body">
+                <h3>{repo.owner.login}</h3>
+                <img class="card-img-top" src={repo.owner.avatar_url} alt="Boomtown's blue logo" style={{ height: 100, width: 100 }}></img>
+                <h6>{repo.owner.url} | {repo.owner.html_url}</h6>
+                <h6>Followers: {repo.owner.followers_url}</h6>
+                <h6>Following: {repo.owner.following_url}</h6>
+                <h6>ID: {repo.owner.id}</h6>
+                <p>Starred: {repo.owner.starred_url}</p>
+                <p>Repos: {repo.owner.repos_url}</p>
+                <p>Events: {repo.owner.events_url}</p>
+                <p>Received Events: {repo.owner.received_events_url}</p>
+                <p>Subscriptions: {repo.owner.subscriptions_url}</p>
+                <p>Organizations: {repo.owner.organizations_url}</p>
+                <p>Gists: {repo.owner.starred_url}</p>
+                <p class="card-subtitle mb-2 text-muted">Type: {repo.owner.type}</p>
+                <p class="card-subtitle mb-2 text-muted">Node ID: {repo.owner.node_id}</p>
+                <p class="card-subtitle mb-2 text-muted">Gravatar ID: {repo.owner.gravatar_id}</p>
+            </div>
+            <div class="card">
+                <p>Homepage: {repo.homepage}
+                <br></br>
+                Size: {repo.size}
+                <br></br>
+                Open Issues: {repo.open_issues}
+                <br></br>
+                Watchers: {repo.watchers}
+                <br></br>
+                Default Brach: {repo.default_branch}
+                <br></br>
+                Stargazers Count: {repo.stargazers_count}
+                <br></br>
+                Watchers Count: {repo.watchers_count}
+                <br></br>
+                Forks Count: {repo.forks_count}
+                <br></br>
+                Open Issues Count: {repo.open_issues_count}
+                <br></br>
+                Language: {repo.language}
+                <br></br>
+                Mirror: {repo.mirror_url}
+                <br></br>
+                Has Issues: {repo.has_issues}
+                <br></br>
+                Has Projects: {repo.has_projects}
+                <br></br>
+                Has Downloads: {repo.has_downloads}
+                <br></br>
+                Has Wiki: {repo.has_wiki}
+                <br></br>
+                Has Pages: {repo.has_pages}
+                <br></br>
+                Has Discussions: {repo.has_discussions}
+                <br></br>
+                Allow Forking: {repo.allow_forking}
+                <br></br>
+                Archived: {repo.archived}
+                <br></br>
+                Disabled: {repo.disabled}
+                <br></br>
+                {/* License: {repo.license} LICENSE IS AN OBJECT!!!*/}
+                <br></br></p>
+            </div>
+            <div class="card">
+                <h4>URLs</h4>
+                <h6>Git URL: {repo.git_url}</h6>
+                <h6>SSH URL: {repo.ssh_url}</h6>
+                <h6>Clone URL: {repo.clone_url}</h6>
+                <h6>SVN URL: {repo.svn_url}</h6>
+                <p>Collaborators: {repo.collaborators_url}</p>
+                <p>Contributors: {repo.contributors_url}</p>
+                <p>Commits: {repo.commits_url}</p>
+                <p>Git Commits: {repo.git_commits_url}</p>
+                <p>Comments: {repo.comments_url}</p>
+                <p>Contents: {repo.contents_url}</p>
+                <p>Downloads: {repo.downloads_url}</p>
+                <p>Issues: {repo.issues_url}</p>
+                <p>Releases: {repo.releases_url}</p>
+                <p>Deployments: {repo.deployments_url}</p>
+                <p>Teams: {repo.teams_url}</p>
+                <p>Branches: {repo.branches_url}</p>
+                <p>Merges: {repo.merges_url}</p>
+                <p>Pulls: {repo.pulls_url}</p>
+                <p>Archive: {repo.archive_url}</p>
+                <p>Hooks: {repo.hooks_url}</p>
+                <p>Forks: {repo.forks_url}</p>
+                <p>Issue Events: {repo.issue_events_url}</p>
+                <p>Issue Comments: {repo.issue_comments_url}</p>
+                <p>Events: {repo.events_url}</p>
+                <p>Tags: {repo.tags_url}</p>
+                <p>Blobs: {repo.blobs_url}</p>
+                <p>Assignees: {repo.assignees_url}</p>
+                <p>Milestones: {repo.milestones_url}</p>
+                <p>Notifications: {repo.notifications_url}</p>
+                <p>Labels: {repo.labels_url}</p>
+                <p>Git Tags: {repo.git_tags_url}</p>
+                <p>Git Refs: {repo.git_refs_url}</p>
+                <p>Compare: {repo.compare_url}</p>
+                <p>Trees: {repo.trees_url}</p>
+                <p>Statuses: {repo.statuses_url}</p>
+                <p>Subscribers: {repo.subscribers_url}</p>
+                <p>Subscritions: {repo.subscriptions_url}</p>
+                <p>Languages: {repo.languages_url}</p>
+                <p>Stargazers: {repo.stargazers_url}</p>
+                <p>Keys: {repo.keys_url}</p>
+                <p class="card-subtitle mb-2 text-muted">Created: {repo.created_at}</p>
+                <p class="card-subtitle mb-2 text-muted">Updated: {repo.updated_at}</p>
+                <p class="card-subtitle mb-2 text-muted">Pushed: {repo.pushed_at}</p>
+            </div>
+            <div class="card">
+                <h1>Permissions</h1>
+                <p>Admin: {repo.permissions.admin}</p>
+                <p>Maintain: {repo.permissions.maintain}</p>
+                <p>Push: {repo.permissions.push}</p>
+                <p>Triage: {repo.permissions.false}</p>
+                <p>Pull: {repo.permissions.pull}</p>
+            </div>
+        </div>
+    )
+
+
+}
+
+export default SingleRepo
